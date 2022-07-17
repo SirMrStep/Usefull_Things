8350b240411a738c7e048351dfacc2562ff536af
Steep <lemikahriman@gmail.com>
2022-07-18 00:45:00 +0200

    Made teleporting trigger PlayerChunkLeaveEvent and PlayerChunkEnterEvent

--- src/main/java/me/steep/usefullthings/events/PlayerChunkEvents/PlayerChunkListener.java
+++ src/main/java/me/steep/usefullthings/events/PlayerChunkEvents/PlayerChunkListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class PlayerChunkListener implements Listener {
 
@@ -14,6 +15,17 @@ public class PlayerChunkListener implements Listener {
 
         Bukkit.getPluginManager().callEvent(new PlayerChunkLeaveEvent(e.getPlayer(), e.getFrom().getChunk()));
         Bukkit.getPluginManager().callEvent(new PlayerChunkEnterEvent(e.getPlayer(), e.getTo().getChunk()));
+
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent e) {
+
+        if(e.isCancelled() || e.getTo() == null || e.getFrom().getChunk().equals(e.getTo().getChunk())) return;
+
+        Bukkit.getPluginManager().callEvent(new PlayerChunkLeaveEvent(e.getPlayer(), e.getFrom().getChunk()));
+        Bukkit.getPluginManager().callEvent(new PlayerChunkEnterEvent(e.getPlayer(), e.getTo().getChunk()));
+
 
     }
 
